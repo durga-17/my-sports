f72540324cbe9ad7087246459f1bedf4c4a7905b
durga-17 <kankipatidurgarao269@gmail.com>
2025-09-10 23:43:07 +0530

    this is my today's code

---
+++ fuctions with return value.js
@@ -0,0 +1,25 @@
+// function expression
+// function anonymous
+// function with parameter and return value
+
+// var a=function Durga(){
+//     console.log("hello! Durga");
+// }
+// a();
+
+// var b=function(){
+//     console.log("i love cricket")
+// }
+// b();
+
+// function score(a,b){
+//     return  a+b
+// }
+// console.log(score(10,20))
+
+function score(a,b){
+    return  a+b
+}
+console.log(score(10,20))
+console.log(score(100,200))
+console.log(score(25,75))
