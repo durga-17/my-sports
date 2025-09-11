ca25ee27a8a7db550c3aae7e9091c164b41408a9
durga-17 <kankipatidurgarao269@gmail.com>
2025-09-11 23:21:22 +0530

    this is today task number finding

---
+++ number.js
@@ -0,0 +1,23 @@
+
+const readline = require("readline");
+
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+
+rl.question("Enter a number: ", function (input) {
+  let number = Number(input);
+
+  if (number > 0) {
+    console.log("The number is positive.");
+  } else if (number < 0) {
+    console.log("The number is negative.");
+  } else {
+    console.log("The number is zero.");
+  }
+
+  rl.close();
+});
